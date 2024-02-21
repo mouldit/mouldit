4d2666e88e23448a92f40e93284231a2caabc9f0
Paul Holsters <paul.holsters@hotmail.com>
2024-02-21 15:20:10 +0100

    impl generate function

--- generate.php
+++ generate.php
@@ -1,6 +1,20 @@
 <?php
-function generate($actions,$path):bool{
+function generate($concepts,$actions,$path):bool{
     $success=true;
-
+    /* STRATEGIE
+     * 1. maak een routes directory
+     * 2. todo maak een aparte SESSIONS variabele met enkel de concepten erin
+     * 3. maak een subdirectory per concept in $concepts
+     * per concept: maak de nodige routefiles aan
+     * de verschillende bestanden per concept zijn als volgt te bepalen:
+     * enkel bij een zuiver subconcept moet de file van de routes voor dat subconcept in dezelfde folder als het
+     * hoofdconcept, edoch dan moeten we een apart algo schrijven die bepaald wat de zuivere subconcepten zijn en
+     * dat is er nu wat over zodus =>
+     * 4. één route file per concept BASTA! alle namen zijn ENKELVOUD!
+     * 5. per file print de verschillende API's
+     * lees app.text
+     * vervang ***blabla*** met de eigenlijke routes zoals per file gecreëerd
+     *
+     * */
     return $success;
 }
