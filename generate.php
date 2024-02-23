a04e6bcf4d3e5b40168a99f13c9a458ca1ef1518
Paul Holsters <paul.holsters@hotmail.com>
2024-02-23 07:03:52 +0100

    impl generate function: app.ts

--- generate.php
+++ generate.php
@@ -1,55 +1,86 @@
 <?php
 function generate($concepts, $actions, $path): bool
 {
+    /*
+     * todo benodigde config data: concepts
+     * import account from './routes/account')
+app.use('/account',account)
+     * */
     if($success = touch($path . '/server.js')){
         $fp=fopen($path . '/server.js', 'w');
         $fileAsStr = file_get_contents('./server.txt');
         fwrite($fp,$fileAsStr,strlen($fileAsStr));
         fclose($fp);
-        if (!file_exists($path . '/routes')) {
-            if ($success = mkdir($path . '/routes')) {
-                for ($i = 0; $i < sizeof($_SESSION['concepts']); $i++) {
-                    if (touch($path . '/routes/' . $_SESSION['concepts'][$i] . '.ts')) {
-                        if ($fp = fopen($path . '/routes/' . $_SESSION['concepts'][$i] . '.ts', 'ab')) {
-                            $fileAsStr = file_get_contents('./route.txt');
-                            $p1 = strstr($fileAsStr, '***replace this with actual route handlers***', true) . "\n";
-                            $p2 = "\n" . trim(substr(strstr($fileAsStr, '***replace this with actual route handlers***'),
-                                    strlen('***replace this with actual route handlers***')));
-                            fwrite($fp, $p1, strlen($p1));
-                            for ($j = 0; $j < sizeof($actions); $j++) {
-                                if (str_contains($actions[$j]->name, $_SESSION['concepts'][$i])) {
-                                    $api1 = 'router.' . $actions[$j]->verb . '(\'' . $_SESSION['concepts'][$i]
-                                        . '/' . $_SESSION['concepts'][$i] . 's\', async (req:any,res:any,next:any)=>{' . "\n\t";
-                                    $api2 = "\n" . '}});' . "\n";
-                                    fwrite($fp, $api1, strlen($api1));
-                                    $body = 'try { return e.select(e.' . ucfirst($_SESSION['concepts'][$i]) . ', () => ({' . "\t" . '
+        if($success = touch($path . '/app.ts')){
+            $fp=fopen($path . '/app.ts', 'w');
+            $fileAsStr = file_get_contents('./app.txt');
+            $app1= strstr($fileAsStr,'***add route imports***',true);
+            $app3= substr(strstr($fileAsStr,'***use routes***'),strlen('***use routes***'));
+            $app2= substr($fileAsStr,strlen($app1)+strlen('***add route imports***'),
+                strlen($fileAsStr)-strlen($app3)-strlen('***use routes***')-strlen($app1)-strlen('***add route imports***'));
+            fwrite($fp,$app1,strlen($app1));
+            global $imports;
+            $imports = "\n";
+            for ($i=0;$i<sizeof($_SESSION['concepts']);$i++){
+                global $imports;
+                $imports.= 'import * as '.$_SESSION['concepts'][$i].' from \'./routes/'.$_SESSION['concepts'][$i].'\''."\n";
+            }
+            fwrite($fp,$imports,strlen($imports));
+            fwrite($fp,$app2,strlen($app2));
+            global $routes;
+            $routes = "\n";
+            for ($i=0;$i<sizeof($_SESSION['concepts']);$i++){
+                global $routes;
+                $routes.= 'app.use(\'/'.$_SESSION['concepts'][$i].'\', '.$_SESSION['concepts'][$i].')'."\n";
+            }
+            fwrite($fp,$routes,strlen($routes));
+            fwrite($fp,$app3,strlen($app3));
+            fclose($fp);
+            if (!file_exists($path . '/routes')) {
+                if ($success = mkdir($path . '/routes')) {
+                    for ($i = 0; $i < sizeof($_SESSION['concepts']); $i++) {
+                        if (touch($path . '/routes/' . $_SESSION['concepts'][$i] . '.ts')) {
+                            if ($fp = fopen($path . '/routes/' . $_SESSION['concepts'][$i] . '.ts', 'ab')) {
+                                $fileAsStr = file_get_contents('./route.txt');
+                                $p1 = strstr($fileAsStr, '***route handlers***', true) . "\n";
+                                $p2 = "\n" . trim(substr(strstr($fileAsStr, '***route handlers***'),
+                                        strlen('***route handlers***')));
+                                fwrite($fp, $p1, strlen($p1));
+                                for ($j = 0; $j < sizeof($actions); $j++) {
+                                    if (str_contains($actions[$j]->name, $_SESSION['concepts'][$i])) {
+                                        $api1 = 'router.' . $actions[$j]->verb . '(\''
+                                            . '/' . $_SESSION['concepts'][$i] . 's\', async (req:any,res:any,next:any)=>{' . "\n\t";
+                                        $api2 = "\n" . '}});' . "\n";
+                                        fwrite($fp, $api1, strlen($api1));
+                                        $body = 'try { return e.select(e.' . ucfirst($_SESSION['concepts'][$i]) . ', () => ({' . "\t" . '
                    ...e.' . ucfirst($_SESSION['concepts'][$i]) . '[\'*\']
                })).run(client);'."\t".'} catch(err){' . "\t" . '
            res.status(500).json({' . "\t\t" . '
                error: err
            })';
-                                    fwrite($fp, $body, strlen($body));
-                                    fwrite($fp, $api2, strlen($api2));
+                                        fwrite($fp, $body, strlen($body));
+                                        fwrite($fp, $api2, strlen($api2));
+                                    }
                                 }
+                                fwrite($fp, $p2, strlen($p2));
+                                fclose($fp);
                             }
-                            fwrite($fp, $p2, strlen($p2));
-                            fclose($fp);
+
+                            /* 5. per file print de verschillende API's
+                * het algo voor elke API is als volgt:
+                * isoleer alle acties voor dit concept,
+                             * per actie schrijf je
+                * /body/
+                * });
+                * Voor de /body/:
+                *
+                * VOORLOPIG is dit voldoende, we gaan ook nog niet de verb in kwestie in detail beoordelen is voor volgende stap
+                *
+
+                }*/
                         }
 
-                        /* 5. per file print de verschillende API's
-            * het algo voor elke API is als volgt:
-            * isoleer alle acties voor dit concept,
-                         * per actie schrijf je
-            * /body/
-            * });
-            * Voor de /body/:
-            *
-            * VOORLOPIG is dit voldoende, we gaan ook nog niet de verb in kwestie in detail beoordelen is voor volgende stap
-            *
-
-            }*/
                     }
-
                 }
             }
         }
