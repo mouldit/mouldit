434bcc0f439653c3183e430682cece8d1f134552
Paul Holsters <paul.holsters@hotmail.com>
2024-02-28 11:03:30 +0100

    bugfix aanmaken concepts array

--- concepts.php
+++ concepts.php
@@ -1,6 +1,6 @@
 <?php
-function getConcepts($schema): array
-{
+function getConcepts($schema): array {
+    // todo fix loopt serieus fout
     $concepts = [];
     $first = strpos($schema,'{')+1;
     $last = strrpos($schema,'}')-1;
@@ -9,18 +9,24 @@ function getConcepts($schema): array
     $regBlocksAsStr = $temp[0];
     $extBlocksAsStr = $temp[1];
     $abstractCodeBlocks = $temp[2];
+    // ok tot hier
     foreach ($abstractCodeBlocks as $ac){
         $data = getConceptData($ac);
         $concept = new Concept($data[0],'abs');
         $concept->addFields($data[1]);
+        //echo '<pre>abs concept => '.print_r($concept, true).'</pre>';
         $concepts[]=$concept;
     }
     foreach ($extBlocksAsStr as $ac){
+        // todo movies en shows worden niet gevonden
         $data = getConceptData($ac);
         $from = strpos($data[0],'extending')+strlen('extending');
+        echo '<pre>concept data for  => '.print_r($data, true).'</pre>';
         $extendsFrom = trim(substr($data[0],$from));
+        //echo 'in => '.$extendsFrom.' from: '.$from;
         for ($i=0;$i<sizeof($concepts);$i++){
             if($concepts[$i]->name===$extendsFrom){
+                echo 'in';
                 $concept = new Concept($data[0],'ext');
                 $concept->setFields($concepts[$i]->fields);
                 $concept->addFields($data[1]);
@@ -35,7 +41,7 @@ function getConcepts($schema): array
         $concept->addFields($data[1]);
         $concepts[]=$concept;
     }
-    echo '<pre>'.print_r($concepts, true).'</pre>';
+
     return $concepts;
 }
 function splitSchema($schemaContent): array
@@ -68,7 +74,7 @@ function getConceptData($codeBlock): array
 {
     $name = trim(substr(strstr($codeBlock,'{',true),strpos($codeBlock,'type')+4));
     if(str_contains($name,'extending')){
-
+        // todo dit is niet goed want je moet weten waarvan het extends om de velden naderhand te kunnen toevoegen
         $name=trim(strstr($name,'extending',true));
         echo $name;
     }
